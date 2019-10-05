6fe61debc531c651910b29ec2d520383757c5cdb
Bala Gayatri Bugatha <balagayathri9@gmail.com>
2019-10-04 23:03:44 -0700

    Updates

---
+++ precache-manifest.0ab1de5120b5d3d886e4511bf3622e94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79486958f3a9de2d7b77ded63af6c430",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "b550b48703d743d5cba4",
+    "url": "/robotfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2eb71eac80ff2c1d221c",
+    "url": "/robotfriends/static/css/main.126360c1.chunk.css"
+  },
+  {
+    "revision": "b550b48703d743d5cba4",
+    "url": "/robotfriends/static/js/2.67777fc4.chunk.js"
+  },
+  {
+    "revision": "2eb71eac80ff2c1d221c",
+    "url": "/robotfriends/static/js/main.9d7b46fa.chunk.js"
+  },
+  {
+    "revision": "5c3251d970998c462fcb",
+    "url": "/robotfriends/static/js/runtime-main.1d5dd8b5.js"
+  }
+]);
